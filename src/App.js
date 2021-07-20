ae76213dd85e96c4e72409b39ffc62c2f70eb88e
EspiraMarvin <espiramarvin@gmail.com>
2021-07-20 15:05:44 +0300

    useState + custom useForm hook

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-// import './App.css';
 import React, { useState } from "react";
+import {useForm} from "./useForm";
 
 // the below function is called when we have a heavy computational task, and we want to only called the func only once
-// when component renders for the 1st time
-function expInState () {
-  return 10
-}
+// when component renders for the 1st time // It runs on the intial value
+// function expInState () {
+//   return 10
+// }
 
 const App = () => {
-  useState(() => expInState())
+  // const [{count, count2 }, setCount] = useState({ count: 10, count2: 20})
+  const [values, handleChange] = useForm({email: '', password: ''})
+  const [values2, handleChange2] = useForm({firstname: '', lastname: ''})
 
   return (
-    <div>Hey {expInState}</div>
+    <div>
+        <input type="text" name="email"
+               value={values.email}
+               onChange={handleChange}
+        />
+        <input type="password" name="password"
+               value={values.password}
+               onChange={handleChange}
+        />
+        <input type="text" name="firstname"
+               value={values.firstname}
+               onChange={handleChange}
+        />
+        <input type="text" name="lastname"
+               value={values.lastname}
+               onChange={handleChange}
+        />
+
+    </div>
   );
 }
 
